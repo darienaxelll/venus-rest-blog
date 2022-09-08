bcc0cdbc0bf888f4cff484619491e38c18f1663f
Darien Salazar <darien.axell.salazar@gmail.com>
2022-09-08 15:08:53 -0500

    Added Jolopy

--- src/main/java/darien/venusrestblog/PostsController.java
+++ src/main/java/darien/venusrestblog/PostsController.java
@@ -1,34 +1,73 @@
 package darien.venusrestblog;
 
 import darien.venusrestblog.data.Post;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+
 @RestController
-@RequestMapping (value = "/api/posts", headers = "Accept=application/json")
+@RequestMapping (value = "/api/posts", produces = "application/json")
 public class PostsController {
-    @GetMapping("/")
+
+    private final List<Post> posts = new ArrayList<>();
+    private long nextId = 1;
+
+    @GetMapping("")
     public List<Post> fetchPosts() {
-        //TODO: Get some post and  return them
-        List<Post> posts = new ArrayList<>();
-        posts.add(new Post(1L, "Post 1", "This  is post one."));
-        posts.add(new Post(2L, "Post 2", "This  is post two."));
         return posts;
     }
 
-    @GetMapping("/{id}")
-    public Post fetchPostsById(@PathVariable long id) {
-        //TODO: Get some post and  return them
-        return switch ((int) id) {
-            case 1 -> new Post(1L, "Post 1", "This  is post one.");
-            case 2 -> new Post(2L, "Post 2", "This  is post two.");
-            default -> throw new RuntimeException("Page not found");
-        };
+    @GetMapping("/id")
+    public Post fetchPostById(@PathVariable long id) {
+        Post post = findPostById(id);
+        if (post == null) {
+            throw new RuntimeException("Sorry I'm lost");
+        }
+        return post;
+    }
+
+    private Post findPostById(long id) {
+        for (Post post: posts) {
+            if (post.getId() == id) {
+                return post;
+            }
+        }
+        return null;
+    }
+
+    @PostMapping(path = "")
+    private void createPost(@RequestBody Post newPost){
+        System.out.println(newPost);
+        newPost.setId(nextId);
+        nextId++;
+        posts.add(newPost);
+    }
+
+    @PutMapping("/id")
+    public void updatePost(@RequestBody Post updatedPost, @PathVariable long id) {
+        Post post = findPostById(id);
+        if(post == null) {
+            System.out.println("Post not found");
+        } else {
+            if(updatedPost.getTitle() != null) {
+                post.setTitle(updatedPost.getTitle());
+            }
+            if(updatedPost.getContent() != null) {
+                post.setContent(updatedPost.getContent());
+            }
+            return;
+        }
+        throw new RuntimeException("Post not found");
+    }
+
+    @DeleteMapping ("/id")
+    public void DeletePostById(@PathVariable long id) {
+        Post post = findPostById(id);
+        if (post != null) {
+            posts.remove(post);
+            return;
+        }
+        throw new RuntimeException("Sorry I'm lost");
     }
 }
---
+++ src/main/resources/static/js/views/PostIndex.js
@@ -0,0 +1,16 @@
+export default function PostIndex(props) {
+    return `
+        <header>
+            <h1>Posts Page</h1>
+        </header>
+        <main>
+            <div>
+                ${props.posts.map(post => `<h3>${post.title}</h3>`).join('')}   
+            </div>
+        </main>
+    `;
+}
+
+export function doStuff() {
+
+}
